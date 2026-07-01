28a07f5e35e0cbd0dcb7e4f348a467002b1f1c2a
wangzexu02 <wangzexu02@baidu.com>
2026-07-01 14:46:03 +0800

    chore: remove unused camera notice

--- src/components/PrivacyNotice.tsx
+++
@@ -1,8 +0,0 @@
-export default function PrivacyNotice() {
-  return (
-    <p className="privacyNotice">
-      手势识别在浏览器本机运行，首次使用会从 MediaPipe CDN 加载识别模型；摄像头画面不会上传。
-      摄像头不可用时，可继续使用手动掷钱完成起卦。
-    </p>
-  );
-}
